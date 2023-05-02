53c22b15925d9c5857bc45e9fff46a88312aff76
Mariano Delfino <marianod2003@hotmail.com>
2023-05-02 16:18:38 -0300

    agrego la ruta y el componente de Terminos y condiciones (#57)

--- frontend/temiro_angular/src/app/app-routing.module.ts
+++ frontend/temiro_angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarrocomprasComponent } from './carrito/carrocompras/carrocompras.component';
 import { BodyComponent } from './home/body/body.component';
+import { TermycondicionesComponent } from './termycondiciones/termycond/termycondiciones.component';
 
 
 const routes: Routes = [
   {path:'carrito', component: CarrocomprasComponent},
   {path:'Home', component: BodyComponent},
+  {path: 'termycond', component: TermycondicionesComponent},
 ];
 
 @NgModule({
