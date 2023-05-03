1a6e5ac1b8fbf0e44cb1b5d1f35e0acd973a110f
Urbina Tatiana <84819160+Tatiana-Urbina@users.noreply.github.com>
2023-05-03 07:47:51 -0300

    Se agregó El ruteo del componente Registro TK09-7 #61 (#62)

--- frontend/temiro_angular/src/app/app-routing.module.ts
+++ frontend/temiro_angular/src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarrocomprasComponent } from './carrito/carrocompras/carrocompras.component';
 import { BodyComponent } from './home/body/body.component';
 import { TermycondicionesComponent } from './termycondiciones/termycond/termycondiciones.component';
+import { SectionComponent } from './home/section/section.component';
+import { RegistroComponent } from './home/registro/registro.component';
 
 
 const routes: Routes = [
+  { path: '', component: BodyComponent},
   {path:'carrito', component: CarrocomprasComponent},
   {path:'Home', component: BodyComponent},
   {path: 'termycond', component: TermycondicionesComponent},
+  {path: 'registro', component: RegistroComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
- 
+
   exports: [RouterModule]
 
 })
