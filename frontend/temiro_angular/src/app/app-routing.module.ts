93a01ff343716be520138004113f79c0ff320561
acostanoeliacba <acostanoeliacba@outlook.com>
2023-05-03 18:55:31 -0300

    Se creo componente faq

--- frontend/temiro_angular/src/app/app-routing.module.ts
+++ frontend/temiro_angular/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { BodyComponent } from './home/body/body.component';
 import { TermycondicionesComponent } from './termycondiciones/termycond/termycondiciones.component';
 import { SectionComponent } from './home/section/section.component';
 import { RegistroComponent } from './home/registro/registro.component';
-
+import {FaqComponent } from './home/faq/faq.component';
 
 const routes: Routes = [
   { path: '', component: BodyComponent},
   {path:'carrito', component: CarrocomprasComponent},
   {path:'Home', component: BodyComponent},
   {path: 'termycond', component: TermycondicionesComponent},
-  {path: 'registro', component: RegistroComponent}
+  {path: 'registro', component: RegistroComponent},
+  {path: 'faq', component: FaqComponent},
 ];
 
 @NgModule({
