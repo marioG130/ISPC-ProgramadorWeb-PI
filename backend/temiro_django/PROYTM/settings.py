0ca4e0b12ed277edb37d634bcaba6c0bba31808a
Mario Guerra <106476346+marioG130@users.noreply.github.com>
2023-05-12 11:53:18 -0300

    creacion modelo django (#108)

--- backend/temiro_django/PROYTM/settings.py
+++ backend/temiro_django/PROYTM/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'PROYTM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'OpticaTeMiroDb',
+        'USER': 'usertm',
+        'PASSWORD': 'passtm.40',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
