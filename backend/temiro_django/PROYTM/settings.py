11cc565524fbcd6d2678790a846bdcf4881a5339
marioG130 <mguerra13@gmail.com>
2023-05-13 09:32:08 -0300

    fix models.py

--- backend/temiro_django/PROYTM/settings.py
+++ backend/temiro_django/PROYTM/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
