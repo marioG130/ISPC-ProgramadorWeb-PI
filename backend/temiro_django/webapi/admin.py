1416dc5e000f9ed0bdf284f7c34aa2ead7c8c1d1
marioG130 <mguerra13@gmail.com>
2023-05-19 20:02:34 -0300

    abm productos y servicios

--- backend/temiro_django/webapi/admin.py
+++ backend/temiro_django/webapi/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('idproducto','descripcion','preciocosto','precio','fechaingreso')
+
+class AbonoAdmin(admin.ModelAdmin):
+    list_display = ('idabono','descripcion','fecha','estado','valor')
+
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('idservicio','descripcion','fecha','estado','valor')
+
+
 admin.site.register(Provincia)
 admin.site.register(Departamento)
 admin.site.register(Ciudad)
@@ -9,7 +20,7 @@ admin.site.register(Usuario)
 admin.site.register(Cliente)
 
 admin.site.register(TipoProducto)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Stock)
 
 admin.site.register(Vendedor)
@@ -18,5 +29,5 @@ admin.site.register(Venta)
 admin.site.register(DetalleVenta)
 
 admin.site.register(Doctor)
-admin.site.register(Abono)
-admin.site.register(Servicio)
+admin.site.register(Abono, AbonoAdmin)
+admin.site.register(Servicio, ServicioAdmin)
