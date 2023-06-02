531b5e796f38ca42b069689971a3d84162ef80cb
Ale <aleprieto@mi.unc.edu.ar>
2023-06-02 01:54:57 -0300

    Mejora de iconos y links hacia las pág respectivas

--- frontend/temiro_angular/src/app/dashboard/dashboard.module.ts
+++ frontend/temiro_angular/src/app/dashboard/dashboard.module.ts
@@ -3,14 +3,12 @@ import { CommonModule }        from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
-import { IndiceComponent }       from './indice/indice.component';
 import { CarrocomprasComponent } from './carrocompras/carrocompras.component';
 import { MisComprasComponent }   from './mis-compras/mis-compras.component';
 
 @NgModule({
   declarations: [
     CarrocomprasComponent,
-    IndiceComponent,
     MisComprasComponent,
   ],
   imports: [
--- frontend/temiro_angular/src/app/dashboardadmin/dashboardadmin.module.ts
+++ frontend/temiro_angular/src/app/dashboardadmin/dashboardadmin.module.ts
@@ -3,11 +3,12 @@ import { CommonModule }        from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
-import { IndiceComponent } from './indice/indice.component';
+
 
 @NgModule({
   declarations: [
-    IndiceComponent ],
+    
+   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
