50bc8f2b41abd178083d780c6012a0254307d625
marioG130 <mguerra13@gmail.com>
2023-06-03 17:01:15 -0300

    metodos para login y registro

--- backend/temiro_django/webapi/serializers.py
+++ backend/temiro_django/webapi/serializers.py
@@ -3,43 +3,7 @@ from rest_framework import serializers
 from .models import *
 
 
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ('idusuario', 'tipousuario', 'usuario', 'password',
-                  'email', 'aceptatc')
-
-
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ('idproducto', 'descripcion', 'idtipoproducto', 'preciocosto',
-                  'precio', 'fechaingreso')
-
-class StockSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Stock
-        fields = ('idproducto', 'cantidad')
-
-class TipoProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TipoProducto
-        fields = ('idtipoproducto', 'descripcion')
-
-class DoctorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Doctor
-        fields = ('iddoctor', 'apellido', 'nombre')
-
-class AbonoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Abono
-        fields = ('idabono', 'idfactura', 'descripcion', 'estado', 'valor', 'fecha')
-
-class ServicioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Servicio
-        fields = ('idservicio', 'idfactura', 'descripcion', 'estado', 'valor', 'fecha')
+# ---- Serializadores referidos a la dirección postal
 
 class ProvinciaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -56,11 +20,42 @@ class CiudadSerializer(serializers.ModelSerializer):
         model = Ciudad
         fields = ('idciudad', 'nombre', 'iddedepartamento')
 
+
+# ---- Serializadores referidos a los clientes
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('idusuario', 'tipousuario', 'usuario', 'password',
+                  'email', 'aceptatc')
+        
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = ('idcliente', 'idusuario', 'apellido', 'nombre', 'dni', 'direccion', 'email',
                   'idciudad', 'telefono', 'celular', 'fechanac')
+
+
+# ---- Serializadores referidos a los productos
+
+class TipoProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoProducto
+        fields = ('idtipoproducto', 'descripcion')
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ('idproducto', 'descripcion', 'idtipoproducto', 'preciocosto',
+                  'precio', 'fechaingreso')
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ('idproducto', 'cantidad')
+
+
+# ---- Serializadores referidos a las ventas
 
 class VendedorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -72,7 +67,6 @@ class FacturaSerializer(serializers.ModelSerializer):
         model = Factura
         fields = ('idfactura', 'tipo', 'cuit', 'total', 'fechaapertura', 'fechacierre',
                   'direccionenvio', 'idciudad')
-        
 
 class VentaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -80,8 +74,26 @@ class VentaSerializer(serializers.ModelSerializer):
         fields = ('idventa', 'idvendedor', 'idcliente', 'idfactura', 'fecha',
                   'descripcion', 'estado')
 
-
 class DetalleVentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = DetalleVenta
         fields = ('iddetalleventa', 'idventa', 'idproducto', 'cantidad', 'descuento')
+
+
+# ---- Serializadores referidos a servicios adicionales
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = ('iddoctor', 'apellido', 'nombre')
+
+class AbonoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Abono
+        fields = ('idabono', 'idfactura', 'descripcion', 'estado', 'valor', 'fecha')
+
+class ServicioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Servicio
+        fields = ('idservicio', 'idfactura', 'descripcion', 'estado', 'valor', 'fecha')
+
