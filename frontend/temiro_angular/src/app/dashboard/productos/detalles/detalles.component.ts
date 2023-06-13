dcd2bfcfe04ae51b9dd6f3a150ca486af4942f76
Mario Guerra <106476346+marioG130@users.noreply.github.com>
2023-06-12 21:47:35 -0300

    producto y detalle desde el backend (#199)

--- frontend/temiro_angular/src/app/dashboard/productos/detalles/detalles.component.ts
+++ frontend/temiro_angular/src/app/dashboard/productos/detalles/detalles.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { listaProductos } from '../lista-productos';
+import { datosExtra } from '../datos-extra';
+
+export class DetProducto {
+    idproducto: number;
+    descripcion: string;
+    detalle: string;
+    especificaciones: string;
+    imagen: string;
+
+    constructor() {
+        this.idproducto = 0;
+        this.descripcion = "";
+        this.detalle = "";
+        this.especificaciones = "";
+        this.imagen = "";
+    }
+}
 
 @Component({
     selector: 'app-detalles',
@@ -9,26 +27,32 @@ import { listaProductos } from '../lista-productos';
 })
 export class DetallesComponent implements OnInit {
 
-    prd!: {
-        name: string;
-        id: number;
-        category: string;
-        identifier: string;
-        description: string;
-        price: number;
-        Stock: string;
-        img: string;
-        qty: number;
-    };
+    private apiUrl = "http://localhost:8000/webapi";
+    public prd: DetProducto;
 
-    constructor(private route: ActivatedRoute,) {
-
+    constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+        this.prd = new DetProducto();
     }
 
     ngOnInit() {
         this.route.paramMap.subscribe(paramsMap => {
-            this.prd = listaProductos[+paramsMap.get('productId')!]
+            this.obtenerProducto(paramsMap.get('idproducto')!);
         })
     }
 
+    obtenerProducto(idprd: string) {
+        let idproducto = Number(idprd);
+        this.http.get<any>(this.apiUrl+"/producto/"+idprd).subscribe(resp => {
+            for (let dx of datosExtra) {
+                if (dx.idproducto==idproducto) {
+                    this.prd.idproducto = dx.idproducto;
+                    this.prd.descripcion = resp.descripcion;
+                    this.prd.detalle = dx.detalle;
+                    this.prd.especificaciones = dx.especificaciones;
+                    this.prd.imagen = "../../assets/img/"+dx.imagen;
+                    break;
+                }
+            }
+        });
+    }
 }
